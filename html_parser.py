f0af007fecd1e18c328c3a662a7cab14e2b5e4d4
kai <zhengk.guo@outlook.com>
2017-07-19 14:45:19 +0800

    fixed bugs

--- html_parser.py
+++ html_parser.py
@@ -6,7 +6,20 @@ import re
 
 # visit the url and get page content
 def get_page_content(url):
-    rsp = requests.get(url)
+    # example douban movie
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "Host": "movie.douban.com",
+        "Pragma": "no-cache",
+        "Upgrade-Insecure-Requests": "1"
+    }
+    rsp = requests.get(url, headers=headers)
+
     html_content = rsp.content
     return html_content
 
@@ -15,13 +28,24 @@ def parser_html(html):
     # example douban movie
     soup = BeautifulSoup(html, "lxml")
     content = soup.find("div", id="content")
-    title = content.find("h1").span.string # 电影名
+    title = content.find("h1").span.get_text() # 电影名
     info = content.find("div", id="info")
-    info = str(info)
-    info = re.sub(r"<.*?>", "", info).strip()
-    for line in info.split("\n"):
-        item = line.split(": ")
-        print "%s----%s" % (item[0], item[1])
+    info = info.get_text().strip()
+    with open("movie.txt", "a") as movie_file:
+    # 电影名,导演名,演员名,类型,制片国家/地区,语言,上映日期
+        A = dict()
+        for line in info.split("\n"):
+            item = line.split(": ")
+            A[item[0]] = item[1]
+        line = u",".join([title or u"",
+                         A.get(u"导演", u""),
+                         A.get(u"主演", u""),
+                         A.get(u"类型", u""),
+                         A.get(u"制片国家/地区", u""),
+                         A.get(u"语言", u""),
+                         A.get(u"上映日期", u""),
+                         A.get(u"片长", u"")])
+        movie_file.write(line.encode(encoding="utf-8") + "\n")
 
 # find links you want to visit
 def find_links(html, watching_list, watched_list):
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import multiprocessing
 import utils
 import html_parser
+import logging
 
 watching_file = "watching.txt"
 watched_file = "watched.txt"
@@ -16,33 +17,41 @@ def main():
             crawl_url(current_url, watching_list, watched_list)
         utils.save_file(watching_file, watching_list)
         utils.save_file(watched_file, watched_list)
-    except:
+    except KeyboardInterrupt as e:
         utils.save_file(watching_file, watching_list)
         utils.save_file(watched_file, watched_list)
+        logging.exception(e)
+    except Exception as e:
+        utils.save_file(watching_file, watching_list)
+        utils.save_file(watched_file, watched_list)
+        logging.exception(e)
 
-def main_muti():
-    watching_list = utils.load_file(watching_file)
-    watched_list = utils.load_file(watched_file)
-    pool = multiprocessing.Pool()
-    try:
-        while len(watching_list) > 0:
-            current_url = watching_list.pop(0)
-            pool.apply_async(crawl_url, (current_url, watching_list, watched_list,))
-        pool.close()
-        pool.join()
-        utils.save_file(watching_file, watching_list)
-        utils.save_file(watched_file, watched_list)
-    except:
-        pool.close()
-        pool.join()
-        utils.save_file(watching_file, watching_list)
-        utils.save_file(watched_file, watched_list)
+# def main_muti():
+#     # todo
+#     watching_list = utils.load_file(watching_file)
+#     watched_list = utils.load_file(watched_file)
+#     pool = multiprocessing.Pool()
+#     try:
+#         while len(watching_list) > 0:
+#             current_url = watching_list.pop(0)
+#             pool.apply_async(crawl_url, (current_url, watching_list, watched_list,))
+#         pool.close()
+#         pool.join()
+#         utils.save_file(watching_file, watching_list)
+#         utils.save_file(watched_file, watched_list)
+#     except KeyboardInterrupt as e:
+#         utils.save_file(watching_file, watching_list)
+#         utils.save_file(watched_file, watched_list)
+#         logging.exception(e)
+#     except:
+#         utils.save_file(watching_file, watching_list)
+#         utils.save_file(watched_file, watched_list)
 
 def crawl_url(url, watching_list, watched_list):
-    print("visiting url: %s, visited %s links" % (url, len(watched_list)))
-    watched_list.append(url)
+    print("visiting url: %s, visited %s links, waiting %s" % (url, len(watched_list), len(watching_list)))
     html = html_parser.get_page_content(url)
     html_parser.parser_html(html)
+    watched_list.append(url)
     html_parser.find_links(html, watching_list, watched_list)
 
 
